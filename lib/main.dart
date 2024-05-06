5a1aafd6b5bd0e983a86aeec073a385632841c58
Kira Resari <kira@tri-tail.com>
2024-05-06 18:02:47 +0200

    Restored dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
       home: const RefinedChatGptLearningTimer(),
     );
   }
